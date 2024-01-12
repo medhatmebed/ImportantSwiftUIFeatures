fb510a6b50b4f6cb4f52e4b54dfbf0de025ee59e
medhatmebed <medhat.mebed@gmail.com>
2024-01-12 05:20:49 +0200

    Initial Commit

---
+++ ImportantSwiftUIFeatures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImportantSwiftUIFeatures
+//
+//  Created by Medhat Mebed on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImportantSwiftUIFeatures/ImportantSwiftUIFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImportantSwiftUIFeaturesApp.swift
+//  ImportantSwiftUIFeatures
+//
+//  Created by Medhat Mebed on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ImportantSwiftUIFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
