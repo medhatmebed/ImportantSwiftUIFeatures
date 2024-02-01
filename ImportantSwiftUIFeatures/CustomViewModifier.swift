0c8e8fc50a68710e15771eec705fadfd11efc730
medhatmebed <medhat.mebed@gmail.com>
2024-02-01 17:25:57 +0200

    spelling

--- ImportantSwiftUIFeatures/CustomViewModifier.swift
+++ ImportantSwiftUIFeatures/CustomViewModifier.swift
@@ -21,7 +21,7 @@ struct DefaultButtonViewModifier: ViewModifier {
             .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
-/// extend the View to add the custom modifiers to the view modifiers list that will make the use of the custome view modifier as .customModifier
+/// extend the View to add the custom modifiers to the view modifiers list that will make the use of the custom view modifier as .customModifier
 extension View {
     
     func withDefaultButtonFormatting(backgroundColor: Color = .blue) -> some View {
